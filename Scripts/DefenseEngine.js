1744398d269ab7a798199178b03aadd385723990
Garrett <garrett.grams@tamu.edu>
2015-11-04 22:11:23 +0000

    time delay

--- Scripts/DefenseEngine.js
+++ Scripts/DefenseEngine.js
@@ -43,7 +43,7 @@ var DefenseEngine = function (game){
     //  'active' means all requested fonts have finished loading
     //  We set a 1 second delay before calling 'createText'.
     //  For some reason if we don't the browser cannot render the text the first time it's created.
-    //active: function() { game.time.events.add(Phaser.Timer.SECOND, createText, this); },
+    active: function() { game.time.events.add(Phaser.Timer.SECOND, OnCreate, this); },
 
     //  The Google Fonts we want to load (specify as many as you like in the array)
     google: {
